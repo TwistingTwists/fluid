d0020b263001debaf18dad711a303987cbc4ec0a
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-02-16 22:07:29 +0530

    feat(circularity): working Circularity logic

--- lib/fluid/model/resources/circularity.ex
+++ lib/fluid/model/resources/circularity.ex
@@ -2,15 +2,31 @@ defmodule Fluid.Model.Circularity do
   @moduledoc """
   Only related to wh details for circularity
   """
-  use Ash.Resource, data_layer: :embedded
+  use Ash.Resource
+  # , data_layer: :embedded
 
   attributes do
-    attribute :inbound_connections, {:array, :struct}, constraints: [items: [instance_of: Fluid.Model.Tag]]
-    attribute :outbound_connections, {:array, :struct}, constraints: [items: [instance_of: Fluid.Model.Tag]]
+    uuid_primary_key(:id)
+    attribute(:wh_id, :string)
+    attribute(:name, :string)
+    # warehouse
+    attribute(:wh, :map)
+    attribute(:inbound_connections, {:array, :struct}, constraints: [items: [instance_of: Fluid.Model.Tag]])
+    attribute(:outbound_connections, {:array, :struct}, constraints: [items: [instance_of: Fluid.Model.Tag]])
 
-    # attribute :inbound_connections,  {:array, Tag}, default: []
-    # attribute :outbound_connections,  {:array, Tag}, default: []
-    attribute :is_feeder_node, :boolean, default: nil
-    attribute :is_unconnected_node, :boolean, default: nil
+    attribute(:is_feeder_node, :boolean, default: nil)
+    attribute(:is_unconnected_node, :boolean, default: nil)
+    attribute(:determinate_classes, {:array, :string}, default: [])
+    attribute(:indeterminate_classes, {:array, :string}, default: [])
+  end
+
+  actions do
+    defaults([:create])
+  end
+
+  code_interface do
+    define_for(Fluid.Model.Api)
+
+    define(:create)
   end
 end
