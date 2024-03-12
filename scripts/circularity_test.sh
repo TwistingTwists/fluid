ff4c3d964b9596dfe8a05409fd8227cf7eaacdf4
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-03-12 16:16:49 +0530

    fix: add script to test group of tests together

--- scripts/circularity_test.sh
+++ scripts/circularity_test.sh
@@ -1,3 +1,4 @@
 bash scripts/test.sh test/fluid/circularity_test.exs:22
 bash scripts/test.sh test/fluid/circularity_test.exs:114
 bash scripts/test.sh test/fluid/circularity_test.exs:195
+bash scripts/test.sh test/fluid/circularity_test.exs:292
