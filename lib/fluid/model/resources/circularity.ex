4802411dd7f3bff6cb3c17db9e9d6a6ccc18e882
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-03-18 18:37:53 +0530

    fix: more refactoring

--- lib/fluid/model/resources/circularity.ex
+++ lib/fluid/model/resources/circularity.ex
@@ -51,9 +51,9 @@ defmodule Fluid.Model.Circularity do
   end
 
   defp do_calculate_feeder_and_unconnected_nodes(
-        %{all: total_wh, indeterminate: list_of_warehouses, determinate: determinate_wh_map},
-        all_tags
-      ) do
+         %{all: total_wh, indeterminate: list_of_warehouses, determinate: determinate_wh_map},
+         all_tags
+       ) do
     {new_wh_acc, tag_acc} =
       for wh <- list_of_warehouses, reduce: {%{}, all_tags} do
         {wh_acc, tag_acc} ->
@@ -90,61 +90,16 @@ defmodule Fluid.Model.Circularity do
     {after_wh_list, after_tags} =
       for {wh_id, wh_map} <- list_of_warehouses_map, reduce: {list_of_warehouses_map, tags_list} do
         {wh_acc, tags_acc} ->
-          case wh_map do
-            %{
-              is_feeder_node: true,
-              is_unconnected_node: false,
-              outbound_connections: outbound_connections
-            } ->
-              # remove all the outbound connections from the warehouse (node)
-              outbound_connections_ids = Enum.map(outbound_connections, & &1.id)
-
-              left_tags =
-                Enum.reject(tags_acc, fn tag ->
-                  tag.id in outbound_connections_ids
-                end)
-
-              # delete the feeder nodes
-              {Map.delete(wh_acc, wh_id), left_tags}
-
-            %{
-              is_feeder_node: false,
-              is_unconnected_node: true,
-              inbound_connections: _inbound_connections,
-              outbound_connections: _outbound_connections
-            } ->
-              # delete the unconnected nodes
-              {Map.delete(wh_acc, wh_id), tags_acc}
-
-            %{is_feeder_node: false, is_unconnected_node: false} ->
-              {wh_acc, tags_acc}
-          end
+          update_wh_map(wh_map, {wh_acc, tags_acc}, wh_id)
       end
 
     # if wh_id is in indeterminate_circularity list => reject it from determinate_wh_map
-    updated_determinate_wh_map =
-      Enum.reject(list_of_warehouses_map, fn {wh_id, _wh_map} ->
-        if after_wh_list[wh_id] do
-          true
-        end
-      end)
-      |> Enum.into(%{})
-
+    updated_determinate_wh_map = update_determinate_map(list_of_warehouses_map, after_wh_list)
     determinate_wh_map = Map.merge(determinate_wh_map, updated_determinate_wh_map)
-
-    # determinate_wh_map
-    # |> Enum.map(fn {_k, v} -> v.name end)
-    # |> yellow("determinate_circularity #{Path.relative_to_cwd(__ENV__.file)}:#{__ENV__.line}")
-
-    # after_wh_list
-    # |> Enum.map(fn {_k, v} -> v.name end)
-    # |> blue("indeterminate_circularity #{Path.relative_to_cwd(__ENV__.file)}:#{__ENV__.line}")
 
     # if no nodes were deleted => do not run_euler_algorithm() further
     if map_size(after_wh_list) < map_size(list_of_warehouses_map) do
-      indeterminate_wh_list =
-        after_wh_list
-        |> Enum.map(fn {_wh_id, %{wh: wh}} -> wh end)
+      indeterminate_wh_list = transform_to_wh_from_wh_map(after_wh_list)
 
       warehouse_current_status = %{
         all: total_wh,
@@ -157,5 +112,67 @@ defmodule Fluid.Model.Circularity do
     else
       %{all: total_wh, indeterminate: after_wh_list, determinate: determinate_wh_map}
     end
+  end
+
+  @doc """
+  There are 3 cases to update the circularity. 
+
+  1. for a feeder node => delete the outbound connections + delete the feeder node 
+  2. for an unconnected node => delete the unconnected node 
+  3. for any other node => don't change anything
+  """
+  def update_wh_map(
+        %{
+          is_feeder_node: true,
+          is_unconnected_node: false,
+          outbound_connections: outbound_connections
+        } = _wh_map,
+        {wh_acc, tags_acc},
+        wh_id
+      ) do
+    # remove all the outbound connections from the warehouse (node)
+    outbound_connections_ids = Enum.map(outbound_connections, & &1.id)
+
+    left_tags =
+      Enum.reject(tags_acc, fn tag ->
+        tag.id in outbound_connections_ids
+      end)
+
+    # delete the feeder nodes
+    {Map.delete(wh_acc, wh_id), left_tags}
+  end
+
+  def update_wh_map(
+        %{
+          is_feeder_node: false,
+          is_unconnected_node: true,
+          inbound_connections: _inbound_connections,
+          outbound_connections: _outbound_connections
+        } = _wh_map,
+        {wh_acc, tags_acc},
+        wh_id
+      ) do
+    {Map.delete(wh_acc, wh_id), tags_acc}
+  end
+
+  def update_wh_map(
+        %{is_feeder_node: false, is_unconnected_node: false} = _wh_map,
+        {wh_acc, tags_acc},
+        _wh_id
+      ) do
+    {wh_acc, tags_acc}
+  end
+
+  defp update_determinate_map(list_of_warehouses_map, after_wh_list) do
+    Enum.reject(list_of_warehouses_map, fn {wh_id, _wh_map} ->
+      if after_wh_list[wh_id] do
+        true
+      end
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp transform_to_wh_from_wh_map(after_wh_list) do
+    Enum.map(after_wh_list, fn {_wh_id, %{wh: wh}} -> wh end)
   end
 end
